015db57aca49523a677fcaa51b7331831df1a8d1
Aleksandr <askuznetsov6996@gmail.com>
2021-10-02 23:44:59 +0300

    task 1 1.5

--- FL17_HW3/homework/js/task1.js
+++ FL17_HW3/homework/js/task1.js
@@ -1,4 +1,21 @@
 // Your code goes here
 do {
-    var initialAmount = +prompt("Enter your initial amount of money");
+    var initialAmount = +prompt("Enter your initial amount of money.");
+    if (isNaN(initialAmount) || (initialAmount < 1000)) {
+        alert('Invalid input data');
+    }
 } while (isNaN(initialAmount) || (initialAmount < 1000));
+
+do {
+    var years = +prompt("Enter number of years. More than 1.");
+    if ((isNaN(years) || (years < 1))) {
+        alert('Invalid input data');
+    }
+} while (isNaN(years) || (years < 1));
+
+do {
+    var percentage = +prompt("Enter number of percentage. Less than 100.");
+    if ((isNaN(percentage) || (percentage > 100))) {
+        alert('Invalid input data');
+    }
+} while (isNaN(percentage) || (percentage > 100));
