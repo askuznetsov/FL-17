b6518894971f6bc3472390d7350ed930cee2e3ed
askuznetsov <askuznetsov6996@gmail.com>
2021-10-18 22:39:59 +0300

    HW9 started

---
+++ FL17_HW9/homework/index.js
@@ -0,0 +1,22 @@
+/* START TASK 1: Your code goes here */
+
+
+const cells = document.querySelectorAll('td');
+
+cells.forEach(cellsItem => {
+    cellsItem.addEventListener('click', function () {
+        cellsItem.classList.add('active');
+    })
+})
+
+
+
+/* END TASK 1 */
+
+/* START TASK 2: Your code goes here */
+
+/* END TASK 2 */
+
+/* START TASK 3: Your code goes here */
+
+/* END TASK 3 */
