8aefe0d53d7e3ebb800d3d0cb07878d255cc130a
okuznetsov <oleksandr.kuznetsov@TX-N-0978.local>
2021-12-02 14:18:22 +0200

    HW4

--- FL17_HW4/homework/task1/js/index.js
+++ FL17_HW4/homework/task1/js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+const body = document.querySelector('body');
+const h1 = document.createElement('h1');
+function sendRequest() {
+    return new Promise((res) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', requestURL);
+        xhr.onload = () => {
+            res(JSON.parse(xhr.response));
+        }
+        xhr.send();
+    })
+}
+
+sendRequest().then(data => {
+    console.log(JSON.stringify(data));
+    data.forEach(element => {
+        const { id, name, username, email } = element;
+        JSON.stringify(element)
+        body.innerHTML += `<p>${id}</p>
+        <p>${name}</p>
+        <p>${username}</p>
+        <p>${email}</p>
+        `
+    });
+});
