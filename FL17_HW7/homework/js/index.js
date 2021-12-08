e58fe6e5fae605296d4a67f4e0c186ad27bc2209
okuznetsov <oleksandr.kuznetsov@TX-N-0978.local>
2021-12-08 21:33:11 +0200

    HW7 fixed eval case

--- FL17_HW7/homework/js/index.js
+++ FL17_HW7/homework/js/index.js
@@ -8,6 +8,6 @@ $(document).ready(function(){
       $("#display").val('');
     }); 
     $(".result").click(function(){
-      $("#display").val(eval($("#display").val()));
+      $("#display").val(Function('return' + $("#display").val()));
     });
   });
