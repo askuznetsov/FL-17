441d430ccebdae9d372829b34ac35cec1a44a3f8
okuznetsov <oleksandr.kuznetsov@TX-N-0978.local>
2021-12-15 21:51:40 +0200

    initial commit HW10

---
+++ FL17_HW10/homework/src/webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable new-cap */
+/* eslint-disable no-undef */
+const miniCss = require('mini-css-extract-plugin');
+module.exports = {
+   entry: '/js/styles.js',
+   output: {
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [{
+         test:/\.(s*)css$/,
+         use: [
+            miniCss.loader,
+            'css-loader',
+            'sass-loader'
+         ]
+      }]
+   },
+   plugins: [
+      new miniCss({
+         filename: 'style.css'
+      })
+   ]
+};
