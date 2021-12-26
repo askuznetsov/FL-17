396ab6debd1b9ad083f56d7677dc3261b7f12790
okuznetsov <oleksandr.kuznetsov@TX-N-0978.local>
2021-12-26 14:46:48 +0200

    added digit replacement

--- FL17_HW7/homework/js/index.js
+++ FL17_HW7/homework/js/index.js
@@ -1,13 +1,35 @@
 // Write your code here
 
-$(document).ready(function(){
-    $('.clc').click(function(){
-      $('#display').val($('#display').val() +$(this).val());
-    });
-    $('.clear').click(function(){
-      $('#display').val('');
-    }); 
-    $('.result').click(function(){
-      $('#display').val(Function('return ' + $('#display').val()));
-    });
+let lastDigit = -1;
+
+$(document).ready(function () {
+  $('.clc').click(function () {
+    if (
+      ($('#display').val().slice(lastDigit) === '*' ||
+        $('#display').val().slice(lastDigit) === '/' ||
+        $('#display').val().slice(lastDigit) === '+' ||
+        $('#display').val().slice(lastDigit) === '-') &&
+      ($(this).val() === '*' ||
+        $(this).val() === '/' ||
+        $(this).val() === '+' ||
+        $(this).val() === '-')
+    ) {
+      let del = [];
+      let i = 0;
+      while (i < $('#display').val().length - 1) {
+        del[i] = $('#display').val()[i];
+        i++;
+      }
+      let res = del.join('');
+      $('#display').val(res + $(this).val());
+    } else {
+      $('#display').val($('#display').val() + $(this).val());
+    }
   });
+  $('.clear').click(function () {
+    $('#display').val('');
+  });
+  $('.result').click(function () {
+    $('#display').val(Function('return ' + $('#display').val()));
+  });
+});
